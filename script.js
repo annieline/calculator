808155e456df04a56954e195cad2975a09e911e9
annieline <80864799+annieline@users.noreply.github.com>
2024-02-02 23:55:03 -0500

    added operate function, included support for string of equations, divide by zero and percentage, css for current operation.

--- script.js
+++ script.js
@@ -1,39 +1,101 @@
 const nums = document.querySelectorAll(".num");
 const operands = document.querySelectorAll(".operand");
+const func = document.querySelectorAll(".func");
 const display = document.getElementById("screen");
 let evalC = [];
-let currentOp;
+let currentOp = null;
+let newInput = null;
 
 nums.forEach(num => num.addEventListener('click', () => {
+    if (newInput) {
+        display.textContent = '';
+        newInput = false;
+    }
     let btnNum = num.innerText;
     display.textContent = (display.innerText + btnNum);
 }));
 
 operands.forEach(oper => oper.addEventListener('click', () => {
-    evalC.push(display.innerText);
-    if (evalC.length == 3){
-        operate([...evalC]);
+    if (!newInput) {
+        evalC.push(display.textContent);
+    }
+    if (evalC.length == 2){
+        operate(Number(evalC[0]), Number(evalC[1]), currentOp);
     }
     switch (oper.id){
-        case 'add':
+        case 'btn-plus':
             currentOp = '+';
-        case 'minus':
+            break;
+        case 'btn-minus':
             currentOp = '-';
-        case 'multiply':
+            break;
+        case 'btn-multiply':
             currentOp = '*';
-        case 'divide':
+            break;
+        case 'btn-divide':
             currentOp = "/";
+            break;
         default:
             currentOp = '';
     }
-    evalC.push(currentOp);
-    clear();
+    newInput = true;
 }));
 
-function operate(one, operand, two) {
+func.forEach(func => func.addEventListener('click', () => {
+    switch(func.id){
+        case 'btn-clear':
+            clear();
+            break;
+        case 'btn-equal':
+            evalC.push(display.textContent);
+            if (evalC.length == 2) {
+                operate(Number(evalC[0]), Number(evalC[1]), currentOp);
+            }
+            else if (evalC.length == 1 && currentOp){
+                operate(Number(evalC[0]), Number(evalC[0]), currentOp);
+            }
+            break;
+        case 'btn-percent':
+            display.textContent = operate(evalC, 100, '/');
+            addNumber();
+    }
+    newInput = true;
+}));
 
+function operate(one, two, op) {
+    if (two == 0 && op == '/') {
+        display.textContent = 'nope!';
+        evalC = [];
+        currentOp = null;
+        return;
+    }
+    let total = 0;
+    switch(op){
+        case '+':
+            total = (one + two);
+            break;
+        case '-':
+            total = (one - two);
+            break;
+        case '*':
+            total = (one * two);
+            break;
+        case '/':
+            total = (one / two);
+            break;
+    }
+    display.textContent = total;
+    addNumber();
 }
 
 function clear(){
-    display.textContent = '';
+    display.textContent = '0';
+    evalC = [];
+    newInput = true;
+    currentOp = null;
+}
+
+function addNumber(){
+    evalC = [];
+    evalC.push(display.textContent);
 }
